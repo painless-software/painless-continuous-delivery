b3b9cc30c561cf8fe3a01c7e1f7507d36ab4ac2b
Peter Bittner <django@bittner.it>
2016-09-17 16:10:14 +0200

    Refactor post-generate hook (make code more portable)
    Add project .gitignore file
    
    Add Credits section to README

--- README.rst
+++ README.rst
@@ -1,18 +1,32 @@
+============================
 Painless Continuous Delivery
 ============================
 
 A cookiecutter for projects with continuous delivery baked in.
 
 Requirements
-------------
+============
 
-Install ``cookiecutter``::
+Install ``cookiecutter``:
+
+.. code-block:: bash
 
     pip install cookiecutter
 
 Usage
------
+=====
 
-Generate a new Cookiecutter template layout::
+Generate a new Cookiecutter template layout:
+
+.. code-block:: bash
 
     cookiecutter gh:painless-software/painless-continuous-delivery
+
+Credits
+=======
+
+This project is brought to you by `Painless Software`_, a best-practice
+consultancy in software development.  Less pain, more fun.
+
+
+.. _Painless Software: https://painless.software/
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,29 +4,46 @@ Post-generate hook for cookiecutter
 import logging
 import os
 import shutil
+import sys
 
-logging.basicConfig(level=logging.DEBUG, format='%(message)s')
-logger = logging.getLogger('post_gen_project')
 
-logger.info('Removing input data folder ...')
-shutil.rmtree('_')
+def shell(command):
+    exit_code = os.system(command)
+    if exit_code:
+        log.error('Project generation failed.')
+        sys.exit(exit_code)
 
-logger.info('Initializing version control ...')
-vcs_info = {
-    'platform_name': '{{ cookiecutter.vcs_platform }}',
-    'platform': '{{ cookiecutter.vcs_platform.lower() }}',
-    'account': '{{ cookiecutter.vcs_account }}',
-    'project': '{{ cookiecutter.project_slug }}',
-}
-vcs_info['remote_uri'] = 'git@{platform}:{account}/{project}.git'.format(**vcs_info)
-vcs_info['web_url'] = 'https://{platform}/{account}/{project}'.format(**vcs_info)
 
-success = os.system(
-    "git init --quiet && "
-    "git add . && "
-    "git commit --quiet -m 'Initial commit by Painless Continuous Delivery' && "
-    "git remote add origin {remote_uri}".format(**vcs_info)
-)
-logger.info("You can now create a project '{project}' on {platform_name}."
-            " {web_url}".format(**vcs_info))
-logger.info('Then push the code to it: $ git push -u origin --all')
+def remove_temporary_files():
+    log.info('Removing input data folder ...')
+    shutil.rmtree('_')
+
+
+def init_version_control():
+    vcs_info = {
+        'platform_name': '{{ cookiecutter.vcs_platform }}',
+        'platform': '{{ cookiecutter.vcs_platform.lower() }}',
+        'account': '{{ cookiecutter.vcs_account }}',
+        'project': '{{ cookiecutter.project_slug }}',
+    }
+    vcs_info['remote_uri'] = \
+        'git@{platform}:{account}/{project}.git'.format(**vcs_info)
+    vcs_info['web_url'] = \
+        'https://{platform}/{account}/{project}'.format(**vcs_info)
+
+    log.info('Initializing version control ...')
+    shell('git init --quiet')
+    shell('git add .')
+    shell('git commit --quiet -m "Initial commit by Painless Continuous Delivery"')
+    shell('git remote add origin {remote_uri}'.format(**vcs_info))
+    log.info("You can now create a project '{project}' on {platform_name}."
+             " {web_url}".format(**vcs_info))
+    log.info('Then push the code to it: $ git push -u origin --all')
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+    log = logging.getLogger('post_gen_project')
+
+    remove_temporary_files()
+    init_version_control()
