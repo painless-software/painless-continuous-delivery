ae220aada88f240a093fc361407efd8706f914f0
Peter Bittner <django@bittner.it>
2016-09-20 11:34:08 +0200

    Generate build matrix for CI services
    Move tests in tests folder (back again)

--- tests/test_project_generation.py
+++ tests/test_project_generation.py
@@ -1,9 +1,13 @@
 """Test generating a project."""
+from os.path import dirname, join
+from filecmp import cmp as compare_files
 
 
 def test_generate_project(cookies):
     """Generate a project with cookiecutter and verify it's complete."""
-    result = cookies.bake()
+    result = cookies.bake(extra_context={
+        'tests': 'flake8,prospector,pylint,py27,py33,py34,py35,pypy',
+    })
 
     assert result.exit_code == 0
     assert result.exception is None
@@ -21,7 +25,8 @@ def test_generate_project(cookies):
 
     tox_ini = result.project.join('tox.ini').readlines(cr=False)
     assert '[tox]' in tox_ini
-    assert 'envlist = flake8,py27,py33,py34,py35,pypy' in tox_ini
+    assert 'envlist =' \
+           ' flake8,prospector,pylint,py27,py33,py34,py35,pypy' in tox_ini
     assert '[testenv]' in tox_ini
     assert '[testenv:flake8]' in tox_ini
     assert '[testenv:prospector]' in tox_ini
@@ -29,3 +34,11 @@ def test_generate_project(cookies):
 
     ci_service_conf = result.project.join('.travis.yml').readlines(cr=False)
     assert 'script: tox' in ci_service_conf
+
+    # ensure this project itself stays up-to-date with the template
+    for filename in ['tox.ini', '.travis.yml']:
+        mother_file = join(dirname(dirname(__file__)), filename)
+        generated_file = result.project.join(filename).strpath
+        assert compare_files(mother_file, generated_file), \
+            "Mother project '{}' not matching template.\n {} != {}".format(
+                filename, mother_file, generated_file)
