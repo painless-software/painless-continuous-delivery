99102b987c9360df46da677175091eb162dcdaa7
Peter Bittner <django@bittner.it>
2017-01-01 11:49:30 +0100

    Ignore Pylint "Invalid constant name" complaints for official Django code

--- {{cookiecutter.project_slug}}/_/frameworks/Django/application/urls.py
+++ {{cookiecutter.project_slug}}/_/frameworks/Django/application/urls.py
@@ -16,6 +16,6 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = [  # pylint: disable=invalid-name
     url(r'^admin/', admin.site.urls),
 ]
