d9720bacceb7abeeb3e79020bb51cb88ef4c3519
Peter Bittner <peter@painless.software>
2019-06-18 18:05:18 +0200

    Some more minor changes from Django 2.2

--- {{cookiecutter.project_slug}}/_/frameworks/Django/manage.py
+++ {{cookiecutter.project_slug}}/_/frameworks/Django/manage.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
+"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
-if __name__ == '__main__':
+
+def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
 
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
