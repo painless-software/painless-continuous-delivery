f95cf4ee0113432f87c35b06569cbdc911221233
Peter Bittner <peter@painless.software>
2019-06-21 17:15:19 +0200

    Add requirements tox target

--- {{cookiecutter.project_slug}}/_/frameworks/Django/application/urls.py
+++ {{cookiecutter.project_slug}}/_/frameworks/Django/application/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns += [
+    urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-    ]
+    ] + urlpatterns
