eb3a2d0cc92e473dcdfbb51909fab7ee8432dd6b
Peter Bittner <peter@painless.software>
2020-03-11 18:49:03 +0100

    Add working draft of field test for Bitbucket
    Rename and add cookiecutter options (à la vcs_project, cloud_project)

--- tests/field/example-bitbucket.sh
+++ tests/field/example-bitbucket.sh
@@ -13,3 +13,47 @@ BASEDIR=$(dirname $0)
 
 source ${BASEDIR}/include/logging.sh
 source ${BASEDIR}/include/api/bitbucket.sh
+
+log 1 'Delete existing merge requests, Git tags, etc.'
+# TODO: Bitbucket API operations
+
+log 2 'Create demo project from scratch and push it'
+tox -e cookiecutter -- \
+    project_name="Example Bitbucket" \
+    project_description="Hello world on Bitbucket" \
+    vcs_platform=Bitbucket.org \
+    vcs_account=appuio \
+    ci_service=bitbucket-pipelines.yml \
+    cloud_platform=APPUiO \
+    cloud_account="demo4501@appuio.ch" \
+    environment_strategy=dedicated \
+    cronjobs=complex \
+    framework=Django \
+    database=Postgres \
+    monitoring=Sentry \
+    license=GPL-3 \
+    push=force \
+    ${*} \
+    --no-input
+
+cd /tmp/painless-generated-projects/example-bitbucket
+
+log 3 'Prepare feature branch'
+# TODO: reuse existing code
+
+log 4 'Add an untested feature'
+# TODO: reuse existing code
+
+log 5 'Create merge request'
+# TODO: Bitbucket API operations
+
+log 6 'Allow pipeline to build and push an image'
+for minutes in $(seq 13 -1 1); do
+    echo "- Waiting... ($minutes' remaining)"
+    sleep 1m
+done
+
+log 7 'Trigger production release'
+git checkout master
+git tag 1.0.0
+git push --tags --force
