1d4663165ba995286820c8059fa799e9fca8012d
Peter Bittner <peter@painless.software>
2020-05-17 22:37:12 +0200

    Verify also Django's manage.py with Pylint by default
    Remove obsolete Flask runserver.py

--- {{cookiecutter.project_slug}}/_/frameworks/Django/manage.py
+++ {{cookiecutter.project_slug}}/_/frameworks/Django/manage.py
@@ -5,8 +5,9 @@ import sys
 
 
 def main():
+    """Main entry point of your Django application"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
-
+    # pylint: disable=import-outside-toplevel
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
