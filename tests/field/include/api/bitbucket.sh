11ea1db1c9754f7aeaf1001a34a52ee58269ed9f
Peter Bittner <peter@painless.software>
2020-08-25 10:04:30 +0200

    Add field test for Java Spring Boot example (#158)

--- tests/field/include/api/bitbucket.sh
+++ tests/field/include/api/bitbucket.sh
@@ -5,11 +5,10 @@
 bitbucket() {
     COMMAND="$1"
     RESOURCE="$2"
-    PROJECT_NAME="appuio%2Fexample-django"
-    PROJECT_URL="https://gitlab.com/api/v4/projects/${PROJECT_NAME}"
+    PROJECT_URL="https://gitlab.com/api/v4/projects/${BITBUCKET_PROJECT_NAME}"
     set -e
     curl --silent \
-        --header "Authorization: Bearer $GITLAB_API_TOKEN" \
+        --header "Authorization: Bearer $BITBUCKET_API_TOKEN" \
         --request $COMMAND \
         "${PROJECT_URL}/${RESOURCE}" "${@:3}"
 }
