4092618bfaa288c9ab0aba9c604bc76b572ddb01
Peter Bittner <peter@painless.software>
2021-04-27 07:50:16 +0200

    Upgrade Django LTS version (2.2. ➜ 3.2)

--- {{cookiecutter.project_slug}}/_/frameworks/Django/manage.py
+++ {{cookiecutter.project_slug}}/_/frameworks/Django/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Main entry point of your Django application"""
+    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'application.settings')
     # pylint: disable=import-outside-toplevel
     from django.core.management import execute_from_command_line
